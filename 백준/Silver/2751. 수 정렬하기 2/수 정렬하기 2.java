f39a88ec6a30cb562fc98f75bc3bba9fdd9d2545
Seo Young Kim <seoyomik@gmail.com>
2025-06-29 14:21:43 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1196 ms, Memory: 160152 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for(int value : list){
+            sb.append(value).append('\n');
+        }
+        
+        System.out.println(sb);
+
+
+    }
+}
