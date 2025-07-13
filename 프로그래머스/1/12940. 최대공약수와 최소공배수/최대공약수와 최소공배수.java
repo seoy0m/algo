ebb98f00a4f3f319448558c5af8af00d40dc388a
Seo Young Kim <seoyomik@gmail.com>
2025-07-13 23:09:24 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.07 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,34 @@
+class Solution {
+    // 최대공약수 구하는 함수 (유클리드 호제법)
+    int gcd(int n, int m) {
+        
+        // 두 수에서 더 큰 수를 n으로 지정
+        if(n < m) {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+        
+        int r;
+        
+        while(m > 0) {
+            r = n % m;
+            n = m;
+            m = r;
+        }
+        return n;
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        
+        // 최대공약수 구하기
+        answer[0] = gcd(n, m);
+        
+        // 최소공배수 구하기
+        answer[1] = n * m / answer[0];
+        
+        return answer;
+    }
+}
