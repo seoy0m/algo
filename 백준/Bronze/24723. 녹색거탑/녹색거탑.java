174a7089da4211a8b8fa6f93729e530da6053de3
Seo Young Kim <seoyomik@gmail.com>
2025-08-05 23:10:44 +0900

    [Bronze IV] Title: 녹색거탑, Time: 68 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+
+
+public class Main {
+
+    static char[][] answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.pow(2, n));
+    }
+}
