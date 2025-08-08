2a898eb0be2b6c4a0f2b9920d847d77ebf918e85
Seo Young Kim <seoyomik@gmail.com>
2025-08-08 10:46:27 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = i + 1;
+		}
+
+		int[] pick = new int[m];
+
+		combination(arr, pick, 0, 0);
+
+	}
+
+	static void combination(int[] arr, int[] sel, int idx, int sidx) {
+		// 기저
+		if (sidx == m) {
+//			System.out.println(Arrays.toString(sel));
+			for(int i =0; i<sel.length;i++) {
+				System.out.print(sel[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		if (idx == n)
+			return;
+
+		// 재귀
+		sel[sidx] = arr[idx];
+		combination(arr, sel, idx +1 , sidx + 1);
+		combination(arr, sel, idx + 1, sidx);
+	}
+}
