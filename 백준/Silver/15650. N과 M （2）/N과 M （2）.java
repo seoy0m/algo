96cdfa828219762914a8e5b77a7133688de0eb62
Seo Young Kim <seoyomik@gmail.com>
2025-08-08 11:19:44 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,9 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, m;
 
+	static int n, m;
+	static int[] arr;
+	static int[] sel;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,35 +15,31 @@ public class Main {
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-
-		int[] arr = new int[n];
-
+		
+		arr= new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = i + 1;
 		}
+		sel =new int[m];
 
-		int[] pick = new int[m];
-
-		combination(arr, pick, 0, 0);
+		com(0, 0);
 
 	}
 
-	static void combination(int[] arr, int[] sel, int idx, int sidx) {
-		// 기저
+	static void com(int idx, int sidx) {
 		if (sidx == m) {
-//			System.out.println(Arrays.toString(sel));
-			for(int i =0; i<sel.length;i++) {
-				System.out.print(sel[i]+" ");
+			for (int i = 0; i < m; i++) {
+				System.out.print(sel[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
 		if (idx == n)
 			return;
-
-		// 재귀
+        
 		sel[sidx] = arr[idx];
-		combination(arr, sel, idx +1 , sidx + 1);
-		combination(arr, sel, idx + 1, sidx);
+		com(idx + 1, sidx + 1);
+		com(idx + 1, sidx);
 	}
+
 }
