a43ce25992cda4eb26def46a0fff2b1ca922b604
Seo Young Kim <seoyomik@gmail.com>
2025-09-05 09:31:46 +0900

    [Bronze II] Title: Hashing, Time: 68 ms, Memory: 11560 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,31 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        String str = br.readLine();
+        String s = br.readLine();
 
-        int r = 31;
-        int m = 1234567891;
-        String s ="abcdefghijklmnopqrstuvwxyz";
-        HashMap<Character, Integer> map = new HashMap<>();
+        final long r = 31L;
+        final long m = 1234567891L;
 
-        for (int i = 0; i <26 ; i++) {
-            map.put(s.charAt(i), i+1);
-        }
+        long hash = 0L;
+        long pow = 1L; // r^0
 
-        int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += (map.get(str.charAt(i))%m)*((int) Math.pow(r,i) %m);
+            int val = s.charAt(i) - 'a' + 1;   // 'a' -> 1, ..., 'z' -> 26
+            hash = (hash + (val * pow) % m) % m;
+            pow = (pow * r) % m;               // 다음 자릿수의 r^i
         }
 
-        System.out.println(sum);
-
-
-    };
-
-
+        System.out.println(hash);
+    }
 }
