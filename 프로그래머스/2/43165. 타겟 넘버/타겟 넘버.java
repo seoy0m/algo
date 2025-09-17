3eaa7035a9ccfae624cb6cb18ba94c8311479e95
Seo Young Kim <seoyomik@gmail.com>
2025-09-17 22:42:59 +0900

    [level 2] Title: 타겟 넘버, Time: 4.40 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,18 +3,20 @@ class Solution {
   
     public int solution(int[] numbers, int target) {  
         dfs(0, numbers, target, 0);  
-        return answer;  
+        return answer;  //타겟 넘버를 만드는 방법의 수
     }  
     
     private void dfs(int dep, int[] numbers, int target, int sum){
+        
         if(dep==numbers.length){
             if(sum==target){
                 answer++;
             }
             return;
         }
-    dfs(dep + 1,numbers,target,sum+numbers[dep]);
-    dfs(dep + 1,numbers,target,sum-numbers[dep]);
+        
+    dfs(dep + 1,numbers,target,sum+numbers[dep]);//plus
+    dfs(dep + 1,numbers,target,sum-numbers[dep]);//minus
     }
         
     
