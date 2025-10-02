d558fd7f49cc1ff0182d9aecb2cf7423edddeba0
Seo Young Kim <seoyomik@gmail.com>
2025-10-02 23:42:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,10 +1,5 @@
 import java.util.*;
-/*
-        1.해당하는 문자가 있으면 그 시작 인덱스랑 그 문자의 수 넣음
-        2.해당하는 숫자가 있으면 그 시작 인덱스랑 그 숫자 넣음
-        3.위를 반복
-        4.만약 해당 인덱스가 이미 넣어져 있으면 스탑모드로 바꾸는 걸로
-        */
+
 class Solution {
     public int solution(String s) {
         HashMap<Integer,Integer> map = new HashMap<>();
