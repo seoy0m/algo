cd67dc89c9df8e81ce8b74069c96219611bc5825
Seo Young Kim <seoyomik@gmail.com>
2026-02-19 18:49:43 +0900

    [level 1] Title: 폰켓몬, Time: 20.17 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i =0 ; i<n; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            
+        int answer = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+            if(map.getOrDefault(nums[i],0)==0){
+                map.put(nums[i],1);
+            } else{
+                map.put(nums[i], map.get(nums[i])+1);
+            }
         }
         
-        int answer = n/2> map.size() ? map.size(): n/2;
-        
+        int n = nums.length/2;
+        if(n>map.size()){
+            answer= map.size();
+        } else {
+            answer = n;
+        }
         
         return answer;
     }
