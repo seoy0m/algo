dcdba1b6f23a1f88b1619b929a05d6f99d96edfd
Seo Young Kim <seoyomik@gmail.com>
2026-02-20 14:38:01 +0900

    [level 2] Title: 전화번호 목록, Time: 308.66 ms, Memory: 250 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,23 @@
-import java.util.*;
-
+import java.util.HashMap;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < phone_book.length; i++) 
-            map.put(phone_book[i], i);
-        
-        //모든 전화번호의 substring이 HashMap에 존재하는지 확인
-        for (int i = 0; i < phone_book.length; i++)
-            for (int j = 0; j < phone_book[i].length(); j++)
-                if (map.containsKey(phone_book[i].substring(0, j)))
+        // 1. 모든 번호를 해시맵에 넣기
+        for (String number : phone_book) {
+            map.put(number, 1);
+        }
+
+        // 2. 각 번호의 접두어가 맵에 있는지 확인
+        for (String number : phone_book) {
+            for (int j = 1; j < number.length(); j++) {
+                // 앞에서부터 한 글자씩 늘려가며 확인
+                if (map.containsKey(number.substring(0, j))) {
                     return false;
-
+                }
+            }
+        }
         return true;
     }
 }
