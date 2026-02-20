11cb7f1a14eec08c4484df7852b41e47f8ce139c
Seo Young Kim <seoyomik@gmail.com>
2026-02-20 12:26:51 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        int n = commands.length;
+        int[] answer = new int[n];
         
-        for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0] - 1;
+        for(int i =0; i< n; i++){
+            int start = commands[i][0];
             int end = commands[i][1];
-
-            int[] copy;
-            if (end == array.length) {
-                copy = Arrays.copyOfRange(array, start, array.length);
-            } else {
-                copy = Arrays.copyOfRange(array, start, end);
+            int k = commands[i][2];
+            
+            List<Integer> list = new ArrayList<>();
+            for(int j = start-1; j< end; j++){
+                list.add(array[j]);
             }
-
-            Arrays.sort(copy);
-            answer[i] = copy[commands[i][2] - 1];
+            Collections.sort(list);
+            answer[i]=list.get(k-1);
         }
-        
         return answer;
     }
 }
