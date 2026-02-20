73cba54ef70c7ff6aaf0c2cc396229cce0f33a00
Seo Young Kim <seoyomik@gmail.com>
2026-02-20 22:28:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.17 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,50 @@
 import java.util.*;
 
 class Solution {
+    static boolean[][] visited;
     
-    static int[] di = {-1,1,0,0};
-    static int[] dj = {0,0,-1,1};
+    static int[] di = {0,0,1,-1};
+    static int[] dj = {1,-1,0,0};
+    
+    static int n, m;
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visited= new boolean[n][m];
+        int answer = -1;
+       
+        n = maps.length;
+        m = maps[0].length;
         
-        answer= bfs(new int[] {0,0,1}, visited, maps,n,m);
+         visited = new boolean[n][m];
+        
+        answer= bfs(0,0,1, maps);
+        
         return answer;
     }
     
-    private int bfs(int[] start, boolean[][] visited, int[][] maps,int n, int m){
+    public int bfs(int i, int j , int cnt, int[][] map){
         Queue<int[]> q = new LinkedList<>();
-        q.add(start);
-        visited[start[0]][start[1]]=true;
-        
+        visited[i][j]=true;
+        q.add(new int[] {i,j,cnt});
         
         while(!q.isEmpty()){
-            int[] curr = q.poll();
+            int[] cur = q.poll();
             
-            if(curr[0]==n-1&&curr[1]==m-1) return curr[2];
-            
-            for(int k=0; k<4; k++){
-                int ni = curr[0]+di[k];
-                int nj = curr[1]+dj[k];
+            for(int k =0 ; k<4 ; k++){
+                int ni = cur[0]+di[k];
+                int nj = cur[1]+dj[k];
                 
-                if(ni<0||ni>=n||nj<0||nj>=m||visited[ni][nj]||maps[ni][nj]==0) 
-                    continue;
+                if(ni < 0 || ni >=n || nj <0 || nj >=m 
+                   || visited[ni][nj] || map[ni][nj]==0) continue;
                 
-                q.add(new int[] {ni,nj,curr[2]+1});
-                visited[ni][nj]=true;
+                if(ni == n-1 && nj == m-1) return cur[2]+1;
+                
+                visited[ni][nj]= true;
+                q.add(new int[] {ni,nj,cur[2]+1});
+                
             }
         }
+        
         return -1;
+        
     }
 }
