0c36f3ce3bf169f91155f059b55a44e6d4b8df4d
Seo Young Kim <seoyomik@gmail.com>
2026-04-21 14:22:27 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 15.05 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,18 @@
 import java.util.*;
-
 class Solution {
     public String solution(String s) {
-        // 1. 공백을 기준으로 문자열 분리
-        String[] parts = s.split(" ");
-        int[] nums = new int[parts.length];
+        String answer = "";
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
         
-        // 2. 문자열 배열을 정수 배열로 변환
-        for (int i = 0; i < parts.length; i++) {
-            nums[i] = Integer.parseInt(parts[i]);
+        for( int i =0; i<str.length; i++){
+            arr[i]= Integer.parseInt(str[i]);
         }
         
-        // 3. 정렬을 통해 최솟값과 최댓값 찾기
-        Arrays.sort(nums);
+        Arrays.sort(arr);
         
-        // 4. 결과 형식에 맞게 문자열 생성 ("최솟값 최댓값")
-        return nums[0] + " " + nums[nums.length - 1];
+        answer+= arr[0]+" "+arr[str.length-1];
+        
+        return answer;
     }
 }
