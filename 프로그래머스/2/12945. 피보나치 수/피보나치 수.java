c36106649c1b5056afc906516f233516083f098d
Seo Young Kim <seoyomik@gmail.com>
2026-04-22 20:35:23 +0900

    [level 2] Title: 피보나치 수, Time: 3.78 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,23 +1,14 @@
 class Solution {
-    
-    static int[] fibo = new int[100000+1];
-    
-    
     public int solution(int n) {
-        int[] fibo = new int[n+1];
-        fibo[1] = 1;
+        int[] dp = new int[n+1];
         
-        if(fibo[n]==0){
-    
-    for(int i =2; i<=n; i++){
-            fibo[i]= (fibo[i-2]+fibo[i-1])%1234567;
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i]= (dp[i-1] + dp[i-2]) % 1234567;
         }
-        }
         
-        int answer = fibo[n];
-        
-        return answer;
+        return dp[n];
     }
-    
-  
 }
